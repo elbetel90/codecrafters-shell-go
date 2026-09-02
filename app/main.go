e1901baecddf8bf811bdedef4d7ce79c6ea1a1de
Elbetel Workineh <elbetelworkineh59@gmail.com>
2026-09-02 16:31:29 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -32,11 +32,8 @@ func main() {
 		if command == "type" {
 			if slices.Contains(built_ins, args[0]) {
 				fmt.Println(args[0] + " is a shell builtin")
-			} else if _, err := exec.LookPath(args[0]); err == nil {
-				cmd := exec.Command(args[0], args[1:]...)
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				cmd.Run()
+			} else if path, err := exec.LookPath(args[0]); err == nil {
+				fmt.Println(args[0] + " is " + path)
 			} else if args[0] == "type" {
 				fmt.Println(args[0] + ": not found")
 			} else {
@@ -48,6 +45,10 @@ func main() {
 			fmt.Println(input[5:])
 		} else {
 			fmt.Println(input + ": command not found")
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
 		}
 
 	}
