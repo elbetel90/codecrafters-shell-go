9fa74b9ec743366fa893ccf2c7f0477bb568105a
Elbetel Workineh <elbetelworkineh59@gmail.com>
2026-09-02 12:10:26 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,9 +12,11 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		input := scanner.Text()
-		fmt.Printf("%s: command not found\n", input)
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+		os.Exit(1)
 	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
