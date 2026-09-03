6356e577a172e957bc2ccbc70ed0b3e7409e3d10
Elbetel Workineh <elbetelworkineh59@gmail.com>
2026-09-03 23:20:35 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,8 +21,46 @@ var built_ins = []string{
 	"cd",
 }
 
+func handlePwd() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "pwd error", err)
+		os.Exit(1)
+	}
+	fmt.Println(dir)
+}
+
+func handleCd(args []string) {
+	arg := args[0]
+	if arg == "~" || strings.HasPrefix(arg, "~/") {
+		homeDir, err := os.UserHomeDir()
+		if err == nil {
+			if arg == "~" {
+				arg = homeDir
+			} else {
+				arg = filepath.Join(homeDir, arg[2:])
+			}
+		}
+	}
+	err := os.Chdir(arg)
+	if err != nil {
+		fmt.Printf("cd: %s: No such file or directory\n", args[0])
+	}
+}
+
+func handleType(args []string) {
+	if slices.Contains(built_ins, args[0]) {
+		fmt.Println(args[0] + " is a shell builtin")
+	} else if path, err := exec.LookPath(args[0]); err == nil {
+		fmt.Println(args[0] + " is " + path)
+	} else if args[0] == "type" {
+		fmt.Println(args[0] + ": not found")
+	} else {
+		fmt.Println(args[0] + ": not found")
+	}
+}
+
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
@@ -35,53 +73,30 @@ func main() {
 		input = strings.TrimSpace(input)
 		args := strings.Split(input, " ")
 		command, args := args[0], args[1:]
-		if command == "exit" {
-			break
-		} else if command == "echo" {
+
+		switch command {
+		case "exit":
+			os.Exit(0)
+		case "echo":
 			fmt.Println(input[5:])
-		} else if command == "pwd" {
-			dir, err := os.Getwd()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "pwd error", err)
-				os.Exit(1)
-			}
-			fmt.Println(dir)
-		} else if command == "cd" {
-			arg := args[0]
+		case "pwd":
+			handlePwd()
+		case "cd":
+			handleCd(args)
+		case "type":
+			handleType(args)
 
-			if arg == "~" || strings.HasPrefix(arg, "~/") {
-				homeDir, err := os.UserHomeDir()
-				if err == nil {
-					if arg == "~" {
-						arg = homeDir
-					} else {
-						arg = filepath.Join(homeDir, arg[2:])
-					}
-				}
-			}
-			err := os.Chdir(arg)
-			if err != nil {
-				fmt.Printf("cd: %s: No such file or directory\n", args[0])
-			}
-		} else if command == "type" {
-			if slices.Contains(built_ins, args[0]) {
-				fmt.Println(args[0] + " is a shell builtin")
-			} else if path, err := exec.LookPath(args[0]); err == nil {
-				fmt.Println(args[0] + " is " + path)
-			} else if args[0] == "type" {
-				fmt.Println(args[0] + ": not found")
+		default:
+			if _, err := exec.LookPath(command); err == nil {
+				cmd := exec.Command(command, args...)
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				cmd.Run()
 			} else {
-				fmt.Println(args[0] + ": not found")
-			}
-		} else if _, err := exec.LookPath(command); err == nil {
-			cmd := exec.Command(command, args...)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Run()
-		} else {
-			fmt.Println(input + ": command not found")
-		}
+				fmt.Println(input + ": command not found")
 
+			}
+		}
 	}
 
 }
